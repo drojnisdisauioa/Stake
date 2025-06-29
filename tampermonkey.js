266bf6c98026fa7ff3b9071ed62e858b0d4fab83
drojnisdisauioa <kebwalol@gmail.com>
2025-06-30 00:35:43 +0100

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Conetic's Stake Script
+// @version      1.0
+// @description  t.me/coneticlarp
+// @author       Conetic - https://t.me/coneticlarp
+// @match        https://stake.com/*
+// @grant        none
+// ==/UserScript==
+
+javascript:(()=>{fetch('https://raw.githubusercontent.com/drojnisdisauioa/Stake/main/bookmark.js').then(r=>r.text()).then(code=>Function(code)());})();

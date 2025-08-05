541b0b39943fdbe9580279e0d8a0820d4ed74ace
drojnisdisauioa <kebwalol@gmail.com>
2025-08-05 21:36:49 +0100

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,19 @@
+Object.assign(document.body.style, {
+  background: '#000',
+  color: '#fff',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  margin: '0',
+  fontSize: '3rem',
+  fontFamily: 'system-ui, sans-serif',
+  textAlign: 'center',
+  lineHeight: '1.4'
+});
+
+document.body.innerHTML = `
+  @zayware scammed you<br>
+  t.me/coneticlarp is the real thing<br><br>
+  Leave a 1 star review forward people here
+`;
